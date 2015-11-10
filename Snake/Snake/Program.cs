c43563b65f655b8643f731b035a4eb3055eee543
markusFrost <andreywystawkin@yandex.ru>
2015-11-10 11:18:36 +0300

    Created Class Point

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,26 +10,43 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            int x1 = 1;
-            int y1 = 3;
-            char sym1 = '*';
+            /* int x1 = 1;
+             int y1 = 3;
+             char sym1 = '*';
+              Draw(x1, y1, sym1);
+             */
 
-            Draw(x1, y1, sym1);
+            Point p1 = new Point();
 
-            int x2 = 4;
+            p1.x = 1;
+            p1.y = 3;
+            p1.sym = '*';
+
+            p1.Draw();
+
+           // Draw(p1.x, p1.y, p1.sym);
+
+           
+
+           /* int x2 = 4;
             int y2 = 5;
             char sym2 = '#';
 
-            Draw(x2, y2, sym2);
+            Draw(x2, y2, sym2);*/
+
+            Point p2 = new Point();
+
+            p2.x = 4;
+            p2.y = 5;
+            p2.sym = '#';
+
+            p2.Draw();
+
+           // Draw(p2.x, p2.y, p2.sym);
 
 
             Console.ReadLine();
         }
 
-        static void Draw(int x, int y, char sym ) // magic function - display point on monitor
-        {
-            Console.SetCursorPosition( x, y );
-            Console.Write( sym );
-        }
     }
 }
