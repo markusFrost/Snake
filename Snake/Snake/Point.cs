d8df79bde8dfb5a9f0c61d8f650cd6287b77400a
markusFrost <andreywystawkin@yandex.ru>
2015-11-10 17:13:53 +0300

    Lesson 9

--- Snake/Snake/Point.cs
+++ Snake/Snake/Point.cs
@@ -24,10 +24,42 @@ namespace Snake
             this.sym = sym;
         }
 
+        public Point(Point p)
+        {
+            this.x = p.x;
+            this.y = p.y;
+            this.sym = p.sym;
+        }
+
+        public void Move(int offset, Direction direction)
+        {
+            if (direction == Direction.RIGHT)
+            {
+                x += offset;
+            }
+            else if (direction == Direction.LEFT)
+            {
+                x -= offset;
+            }
+            else if (direction == Direction.UP)
+            {
+                y -= offset;
+            }
+            else if (direction == Direction.DOWN)
+            {
+                y += offset;
+            }
+        }
+
         public void Draw()
         {
             Console.SetCursorPosition(x, y);
             Console.Write(sym);
+        }
+
+        public override string ToString()
+        {
+            return x + ", " + y + ", " + sym + "";
         }
     }
 }
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -35,6 +35,9 @@ namespace Snake
             leftLine.Draw();
             rigthLine.Draw();
 
+            Point p = new Point(4, 5, '*');
+            Snake snake = new Snake( p, 4, Direction.RIGHT );
+            snake.Draw();
 
 
             Console.ReadLine();
