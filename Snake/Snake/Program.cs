300b50214b1f6329049b256dc86b4d4919c0c9e4
markusFrost <andreywystawkin@yandex.ru>
2015-11-10 11:35:56 +0300

    Encapsulation

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -9,41 +9,15 @@ namespace Snake
     class Program
     {
         static void Main(string[] args)
-        {
-            /* int x1 = 1;
-             int y1 = 3;
-             char sym1 = '*';
-              Draw(x1, y1, sym1);
-             */
-
-            Point p1 = new Point();
-
-            p1.x = 1;
-            p1.y = 3;
-            p1.sym = '*';
-
+        {           
+            Point p1 = new Point(1, 3, '*' );
             p1.Draw();
 
-           // Draw(p1.x, p1.y, p1.sym);
-
-           
-
-           /* int x2 = 4;
-            int y2 = 5;
-            char sym2 = '#';
-
-            Draw(x2, y2, sym2);*/
-
-            Point p2 = new Point();
-
-            p2.x = 4;
-            p2.y = 5;
-            p2.sym = '#';
-
+            Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-           // Draw(p2.x, p2.y, p2.sym);
-
+            //Инкапсуляция - свойство системы, позволяющее объединить данные и методы, работающие 
+            //с ними в классе и скрыть все делати реализации от пользователя
 
             Console.ReadLine();
         }
